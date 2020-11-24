5d9eab43d1dfaaed8e4475cbf1b0c8f077eac72d
victor13alvarez <55243827+victor13alvarez@users.noreply.github.com>
2020-11-24 15:04:52 +0100

    PLayerModel

---
+++ Assets/PlayerAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    Animator _pAnim;
+    public bool Pray;
+    private void Awake()
+    {
+        _pAnim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pAnim.SetBool("Pray", Pray);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
